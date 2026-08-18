9beb8888f35121309c24ffb0a3c1b31dc3eb5139
saurbu <ss1742@dseu.ac.in>
2026-08-19 04:45:16 +0530

    user

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/car", carRoutes);
-app.use("/api/auth", authRouter)
+app.use("/api/auth", AuthRouter)
 app.use("/api/user", userRouter)
 
 const startServer = async () => {
